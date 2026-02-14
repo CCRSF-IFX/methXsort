b013b5c139f5e0928f4d43498356f177cb56f9a3
xies4 <xies4@nih.gov>
2026-02-14 12:08:13 -0500

    add metadata in __init__.py

---
+++ methxsort/__init__.py
@@ -0,0 +1,11 @@
+"""
+methXsort - Sort bisulfite-/enzymatic-converted reads from xenograft experiments
+"""
+
+__version__ = "0.2.0"
+__author__ = "Shaojun Xie"
+__email__ = "xies4@nih.gov"
+
+from . import core
+
+__all__ = ['core']
